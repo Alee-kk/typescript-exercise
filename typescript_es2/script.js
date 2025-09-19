7d2856cdd53b80dc5436f9e8b1c396a0bcedbeca
AleezinhoCAPUDO <alexandrekk21@gmail.com>
2025-09-19 09:29:03 -0300

    0909

--- typescript_es2/script.js
+++ typescript_es2/script.js
@@ -8,7 +8,7 @@
         function triple(value) {
     return value * 3;
 }
-function greetTripled(greeting, value) {
-            console.log("".concat(greeting, ", ").concat(triple(value), "!"));
+                        function greetTripled(greeting, value) {
+                                                    console.log("".concat(greeting, ", ").concat(triple(value), "!"));
 }
 greetTripled("Hiya", 5)         ;
